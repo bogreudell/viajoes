f684595c129fde91b363926c18c554650e1aa228
Daniel Bogre Udell <daniel@wikitongues.org>
2021-03-18 23:53:30 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<script src="https://kit.fontawesome.com/b108d252b7.js" crossorigin="anonymous"></script>
+<?php wp_head(); ?>
+</head>
