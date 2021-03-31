41ada1b0d63bdf66eef14823a408234d0207bcc4
Daniel Bogre Udell <daniel@wikitongues.org>
2021-03-30 23:36:52 -0400

    started typography

--- header.php
+++ header.php
@@ -3,6 +3,12 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width" />
+
+	<!-- typefaces from google -->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&family=Source+Sans+Pro:wght@300;400;600&display=swap" rel="stylesheet">
+
+	<!-- fontawesome for icons -->
 	<script src="https://kit.fontawesome.com/b108d252b7.js" crossorigin="anonymous"></script>
 	<?php wp_head(); ?>
 </head>
