2fa53b9def8276cd4a7c6976358878727ff00dad
Daniel Bogre Udell <daniel@wikitongues.org>
2021-04-07 22:34:55 -0400

    more css

--- components/service.php
+++ components/service.php
@@ -1,18 +1,44 @@
 <?php if( have_rows('service') ): ?>
-<div class="vj_services">
 	<?php while( have_rows('service') ): the_row(); ?>
+
+	<?php $video_embed = get_sub_field('video_embed'); ?>
+	<?php $first_image = get_sub_field('first_image'); ?>
+	<?php $second_image = get_sub_field('second_image'); ?>
+	<?php $full_image = get_sub_field('image'); ?>
+		
 	<div class="vj_service">
 		<h1 class="vj_service__title">
 			<?php the_sub_field('title'); ?>
 		</h1>
-		<p class="vj_service__copy">
+		<div class="vj_service__copy">
 			<?php the_sub_field('copy'); ?>
-		</p>
-		<?php the_sub_field('video_embed'); ?>
+		</div>
+
+		<!-- if video embed exists, display markup -->
+		<?php if( $video_embed): ?>
+		<div class="vj_service__iframewrap">
+			<?php echo $video_embed; ?>
+		</div>
 		<div class="vj_service__videocaption">
 			<?php the_sub_field('video_caption'); ?>
 		</div>
+		<?php endif; ?>
+
+		<!-- if 'first image' exists, display markup -->
+		<?php if( $first_image ): ?>
+		<div class="vj_service__images">
+			<img src="<?php echo $first_image['url']; ?>" alt="<?php echo $first_image['alt']; ?>">
+			<img src="<?php echo $second_image['url']; ?>" alt="<?php echo $second_image['alt']; ?>">
+			<div class="clear"></div>
+		</div>
+		<?php endif; ?>
+
+		<!-- if 'full image' exists, display markup -->
+		<?php if( $full_image ): ?>
+		<div class="vj_service__fullimage">
+			<img src="<?php echo $full_image['url']; ?>" alt="<?php echo $full_image['alt']; ?>">
+		</div>
+		<?php endif;?>
 	</div>
 	<?php endwhile; ?> 
-</div>
 <?php endif; ?>
--- template-services.php
+++ template-services.php
@@ -6,10 +6,16 @@ if( have_rows('primary_content') ){
 	while( have_rows('primary_content') ){
 		the_row();
 
-		if( get_row_layout() == 'services' ){
+		if( get_row_layout() == 'services_video_embed' ){
 			include( locate_template('components/service.php') );
 
-		} elseif( get_row_layout() == 'banner' ){
+		} elseif( get_row_layout() == 'services_double_image' ){
+			include( locate_template('components/service.php') ); 
+
+		} elseif( get_row_layout() == 'services_full_image' ){
+			include( locate_template('components/service.php') );
+
+		}elseif( get_row_layout() == 'banner' ){
 			include( locate_template('components/content-banner.php') );
 
 		} elseif( get_row_layout() == 'offerings' ){
