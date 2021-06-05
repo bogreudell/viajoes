0cfc8aa9a0578c65de6b75396ac5a6aee34bf7ed
Daniel Bogre Udell <daniel@wikitongues.org>
2021-06-05 13:31:48 -0400

    more revisions

--- template-sustainability.php
+++ template-sustainability.php
@@ -5,11 +5,16 @@ include( locate_template('components/banner-image.php') );
 $the_query = new WP_Query( array('post_type'=>'post') );
 
 if( $the_query->have_posts() ){
+
+	echo '<div class="vj_blogpreview__wrapper">';
+
 	while( $the_query->have_posts() ){
 		$the_query->the_post();
 
 		include( locate_template('components/post-preview.php') );
 	}
+
+	echo '</div>';
 }
 
 get_footer(); ?>
